5d1927d81b324e32a99221dba481d869409ca03c
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-02-23 15:39:10 +0530

    Update S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "admin-app-project-terraform-state-23"
+    bucket = "admin-app-project-terraform-state-07"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
