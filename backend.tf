327e4af717c89d40b703ad7477f88f9b34a2fdc7
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-02-23 18:35:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "admin-app-project-terraform-state-07"
+    bucket = "young-minds-app-project-terraform-state-07"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
