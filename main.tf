298002ed1428a4e2764e74ea3f48737761bb8d1b
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-03-02 21:38:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "vpc-0081b187c0e9b09fd" {
+resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
   instance_tenancy = "default"
   tags = {
