dfc05dd79b3be4f3bf07dbdf712e729eb35bb958
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-03-02 21:25:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-server-app-project-terraform-state-07"
+    bucket = "jenkins-terraform-1264"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
